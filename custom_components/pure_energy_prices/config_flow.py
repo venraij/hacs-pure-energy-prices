62801a5040a8966fe67de331911d07599895bad8
VenRaij <me@nickvanraaij.com>
2026-08-01 13:04:08 +0200

    few little fixes

--- custom_components/pure_energy_prices/config_flow.py
+++ custom_components/pure_energy_prices/config_flow.py
@@ -40,7 +40,7 @@ class PureEnergyPricesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     int, vol.Range(min=60, max=86400)
                 ),
                 vol.Optional(CONF_COMMODITY, default="electricity"): vol.In(["electricity", "gas"]), # type: ignore
-                vol.Optional("unit_of_measurement", default="€/kWh"): vol.In(["€/kWh", "€/m³"]), # type: ignore
+                vol.Optional(CONF_UNIT_OF_MEASUREMENT, default="€/kWh"): vol.In(["€/kWh", "€/m³"]) # type: ignore
             }
         )
 
