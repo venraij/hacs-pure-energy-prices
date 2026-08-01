266e6fe67cc3ce6b5190a5ee36715ffcd309cecc
VenRaij <me@nickvanraaij.com>
2026-08-01 12:46:15 +0200

    bit of cleanup

--- custom_components/pure_energy_prices/sensor.py
+++ custom_components/pure_energy_prices/sensor.py
@@ -1,16 +1,14 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
 import logging
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import (
     CoordinatorEntity,
 )
 from homeassistant.components.sensor import SensorEntity, SensorStateClass
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from voluptuous import Any
 
 from custom_components.pure_energy_prices.const import DOMAIN
 from custom_components.pure_energy_prices.coordinator import PureEnergyCoordinator
